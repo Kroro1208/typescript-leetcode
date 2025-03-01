55769ecb607673110b9bb35ed9b8ff309346c6f6
Kroro1208 <yamagata_7580@yahoo.co.jp>
2025-03-01 15:20:55 +0900

    objectの型をRecord使用した型に修正

--- Longest-Substring-Non-Repeating/main.ts
+++ Longest-Substring-Non-Repeating/main.ts
@@ -1,17 +1,18 @@
 // 最長の重複のない部分文字列の長さを出力する
+type TextObj = Record<string, number>; // {[key: string]: number} と同じ
 function longestSubstrings(text: string): number {
-  const textObj: { [key: string]: number } = {};
+  const textObj: TextObj = {};
   let left = 0;
   let right = 0;
   let answer = 0;
 
   for (let i = 0; i < text.length; i++) {
     if (text[i] in textObj) {
-      left = Math.max(left, textObj[text[i]] + 1);
+      left = Math.max(left, textObj[text[i]] + 1); // 重複した文字があった場合、leftを前に進める処理
     }
     right += 1;
     textObj[text[i]] = i;
-    answer = Math.max(right - left, answer);
+    answer = Math.max(right - left, answer); // 探索結果の最大値を取る
   }
 
   return answer;
